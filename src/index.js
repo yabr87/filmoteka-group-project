58b4e93e2e1ff9bc080f2797613d51454e8228ec
yabr87 <108272773+yabr87@users.noreply.github.com>
2022-12-19 22:21:03 +0200

    start project

--- src/index.js
+++ src/index.js
@@ -2,16 +2,3 @@ import ApiService from './js/fetchservice';
 import { refs } from './js/refs';
 import { paginationHomePage } from './js/paginationhomepage';
 import { onFilmClick } from './js/render/rendermodal';
-import { renderHomePage } from './js/render/renderhomepage';
-
-const apiService = new ApiService();
-
-async function start() {
-  let trendingFilms = await apiService.fetchTrendingFilms();
-  renderHomePage(trendingFilms);
-  paginationHomePage();
-}
-
-start();
-
-refs.mainLibrary.addEventListener('click', onFilmClick);
--- src/js/render/rendermylybrary.js
+++ src/js/render/rendermylybrary.js
@@ -1,24 +1,3 @@
 import { refs } from '../refs';
 import ApiService from '../fetchservice';
 const apiService = new ApiService();
-
-const murkupMyLybrary = ({
-  poster_path,
-  title,
-  original_title,
-  genre,
-  release_date,
-  vote_average,
-}) => {
-  return `<li class="library-item" data-id="${id}">
-      
-        <img class="film-poster" src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${original_title}"/>
-        <p class="film-name">${title}</p>
-         <p class="film-descr">
-          ${genre.join(', ')} | ${release_date.slice(0, 4)}
-        </p>
-        <p class="vote">${vote_average} </p>
-
-      
-    </li>`;
-};
