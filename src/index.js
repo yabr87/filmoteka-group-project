14e2081c2af11b81d4c34a9ac2879c4576654d3a
AlonaMaliarchyk <alenasymorot@gmail.com>
2022-12-20 20:39:44 +0400

    movieService

--- src/index.js
+++ src/index.js
@@ -2,3 +2,37 @@ import ApiService from './js/fetchservice';
 import { refs } from './js/refs';
 import { paginationHomePage } from './js/paginationhomepage';
 import { onFilmClick } from './js/render/rendermodal';
+import { MovieService } from './js/fetchservice';
+
+let movieService = new MovieService();
+
+movieService.getTrending(
+    {
+        mediaType: "all",
+        timeWindow: "week"
+    }
+).then(res => {
+    console.log(res);
+});
+
+movieService.search(
+    {
+        query:"gladiator",
+        page:"1",
+        include_adult:"false",
+        region:"USA",
+        year:"2005",
+        primary_release_year: "2005-06-07",
+        language:"en-US"
+    }
+).then(res => {
+    console.log(res);
+})
+
+movieService.getMovieDetails(76600).then(res => {
+    console.log(res);
+})
+
+movieService.getMovieVideos(76600).then(res => {
+    console.log(res);
+})
--- src/js/fetchservice.js
+++ src/js/fetchservice.js
@@ -1,1 +1,64 @@
 import axios from 'axios';
+
+export class MovieService {
+
+    #API_KEY;
+
+    constructor() {
+        this.#API_KEY = '1db949d546d8184041e5d93169d90d9f';
+    }
+
+    getTrending(params = {}) {
+        let url = `https://api.themoviedb.org/3/trending`;
+        if ('mediaType' in params) { 
+            url = url + `/${params.mediaType}`; 
+        }
+        if ('timeWindow' in params) { 
+            url = url + `/${params.timeWindow}`; 
+        }
+        url = `${url}?api_key=${this.#API_KEY}`;
+        console.log(url);
+        return axios.get(url);
+    };
+
+    search(searhTerm) {
+        let url = `https://api.themoviedb.org/3/search/movie?api_key=${this.#API_KEY}`;
+        if ('query' in searhTerm) { 
+            url = url + `&query=${searhTerm.query}`; 
+        }
+        if ('page' in searhTerm) { 
+            url = url + `&page=${searhTerm.page}`; 
+        }
+        if ('include_adult' in searhTerm) { 
+            url = url + `&include_adult=${searhTerm.include_adult}`; 
+        }
+        if ('region' in searhTerm) { 
+            url = url + `&region=${searhTerm.region}`; 
+        }
+        if ('year' in searhTerm) { 
+            url = url + `&year=${searhTerm.year}`; 
+        }
+        if ('primary_release_year' in searhTerm) { 
+            url = url + `&release_year=${searhTerm.primary_release_year}`; 
+        }
+        if ('language' in searhTerm) { 
+            url = url + `&language=${searhTerm.language}`; 
+        }
+        
+        console.log(url);
+        return axios.get(url);
+    };
+
+    getMovieDetails(movieId) {
+        let url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${this.#API_KEY}`;
+        return axios.get(url);
+    };
+
+    getMovieVideos(movieId) {
+        let url = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${this.#API_KEY}`;
+        console.log(url);
+        return axios.get(url);
+    };
+
+}
+
