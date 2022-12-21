83860620be5c625a15d1b7c41ac6d82605b50299
AlonaMaliarchyk <alenasymorot@gmail.com>
2022-12-21 23:59:12 +0400

    genre

--- src/js/fetchservice.js
+++ src/js/fetchservice.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import genresJson from "./genres.json" 
 
 export class MovieService {
     #BASE_URL;
@@ -9,6 +10,8 @@ export class MovieService {
     #GET_BY_GENRE;
     #POSTER_PATH;
 
+    #genresMap;
+
 
     constructor() {
         this.#API_KEY = '1db949d546d8184041e5d93169d90d9f';
@@ -18,15 +21,14 @@ export class MovieService {
         this.#ID_MOVIE_URL = `${this.#BASE_URL}/movie/`;
         this.#GET_BY_GENRE = `${this.#BASE_URL}/genre/movie/list`;
         this.#POSTER_PATH = `https://image.tmdb.org/t/p/original`;
+        this.#genresMap = this.getAllGenres();
     }
 
     async getTrending(page) {
         try {
             const { data } = await axios.get(
                 `${this.#TREND_URL}?api_key=${this.#API_KEY}&page=${page}`,
-                console.log(`${this.#TREND_URL}?api_key=${this.#API_KEY}&page=${page}`)
             );
-            console.log(data);
             return data;
         } catch (error) {
             console.error('Smth wrong with api get full trends' + error);
@@ -37,9 +39,7 @@ export class MovieService {
         try {
             const { data } = await axios.get(
                 `${this.#SEARCH_URL}?api_key=${this.#API_KEY}&query=${text}&page=${page}`,
-                console.log(`${this.#SEARCH_URL}?api_key=${this.#API_KEY}&query=${text}&page=${page}`)
             );
-            console.log(data);
             return data;
         } catch (error) {
             console.error('Smth wrong with api get full trends' + error);
@@ -50,9 +50,7 @@ export class MovieService {
         try {
             const { data } = await axios.get(
                 `${this.#ID_MOVIE_URL}${movieId}?api_key=${this.#API_KEY}&append_to_response=videos`,
-                console.log(`${this.#ID_MOVIE_URL}${movieId}?api_key=${this.#API_KEY}&append_to_response=videos`)
             );
-            console.log(data);
             return data;
         } catch (error) {
             console.error('Smth wrong with api get full trends' + error);
@@ -64,10 +62,8 @@ export class MovieService {
     async getByGenre(genre) {
         try {
             const { data } = await axios.get(
-                `${this.#GET_BY_GENRE}?api_key=${this.#API_KEY}&append_to_response=videos`,
-                console.log(`${this.#GET_BY_GENRE}?api_key=${this.#API_KEY}&append_to_response=videos`)
+                `${this.#GET_BY_GENRE}?api_key=${this.#API_KEY}`,
             );
-            console.log(data);
             return data;
         } catch (error) {
             console.error('Smth wrong with api get full trends' + error);
@@ -76,5 +72,25 @@ export class MovieService {
 
     getPosterPath(imagePath) {
         return this.#POSTER_PATH + imagePath;
+    }
+
+    getGenreById(id) {
+        return this.#genresMap.get(id);
+    }
+
+    getGenresByIds(ids) {
+        const genres = [];
+        ids.forEach(id => {
+            genres.push(this.getGenreById(id));
+        });
+        return genres;
+    }
+
+    getAllGenres() {
+        const genresMap = new Map();
+        genresJson.genres.map(entry => {
+            genresMap.set(entry.id, entry.name);    
+        })
+        return genresMap;
     }
 }
--- src/js/search.js
+++ src/js/search.js
@@ -17,7 +17,7 @@ async function handleSubmit(event) {
 
 function createGalleryMarckup(response) {
     return response
-        .map(({poster_path, backdrop_path, title, release_date, genre}) => {
+        .map(({poster_path, backdrop_path, title, release_date, genre_ids}) => {
             return `
             
                 <a href="${movieService.getPosterPath(backdrop_path)}">
@@ -28,11 +28,15 @@ function createGalleryMarckup(response) {
                     <b >${title}</b>
                     </p>
                     <p class="info-item">
-                    <b>${genre}</b>
+                    <b>${movieService.getGenresByIds(genre_ids)}</b>
                     </p>
                     <p class="info-item">
-                    <b >${release_date}</b>
+                    <b >${extractYear(release_date)}</b>
                     </p>
                 </div>
         `}).join('');    
 };
+
+function extractYear(fullDate) {
+    return fullDate.slice(0, 4);
+}
