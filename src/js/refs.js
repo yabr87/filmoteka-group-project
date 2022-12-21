ef8568e821234741072f365e6ba53e7b899e1547
Vladislav Mararenko <mararenko.vladislav@gmail.com>
2022-12-21 15:03:47 +0200

    Firebase added

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,5 @@
 export const refs = {
   mainLibrary: document.querySelector('.js-main-library'),
   myLibrary: document.querySelector('.js-my-library'),
+  authBtnWrap: document.querySelector('.auth-link-wrap'),
 };
