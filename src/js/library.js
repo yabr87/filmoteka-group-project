9cda6f16f5de7b82ea57d6a5bc57c8680faf10b3
yabr87 <108272773+yabr87@users.noreply.github.com>
2022-12-25 02:57:13 +0200

    Update library.js

--- src/js/library.js
+++ src/js/library.js
@@ -11,15 +11,6 @@ import { Loading } from 'notiflix';
 
 import { Loading } from 'notiflix';
 
-const onEmptyLibraryPageLoading = () => {
-  Loading.circle('Add film to the list first');
-  Loading.remove(5000);
-  setTimeout(() => {
-    return (location.href = '../index.html');
-  }, 5000);
-};
-onEmptyLibraryPageLoading();
-
 const onHeaderBtnsClick = e => {
   const btnType = e.target.dataset.type;
   if (!btnType) {
